5eda48c1ebe4cfba6b4f94e5f2cf69b02db5792b
Sydney Ellen Haggard <Aussie@Sydneys-MacBook-Air.local>
2016-09-21 10:29:46 -0400

    Added Javascript

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,87 @@
+// var num = 7;
+// var newNum = 5; //this is camel casing
+// var newer_num = 10; //this is snake casing
+
+// //console.log prints to the console
+// //remember to his command + b to take the code to the bottom console
+
+// //opperations with console.log
+// console.log(348 + 8543);
+
+// //TODO - make a subtraction, multiplication, anf division console.log()
+
+// //subtraction
+// console.log(15 - 12);
+// //multipliction
+// console.log(3 * 4);
+// //division
+// console.log(300 / 10);
+
+// //Modulus Operator
+// console.log(10 % 5); //returns zero
+// console.log(4 % 3); //returns 1
+// console.log(5 % 10);//returns 5
+// console.log(5 % 6);//returns 5
+
+// //Make a modulus that returns zero and another that returns a remainder
+
+// console.log(6 % 5);//returns 1
+// console.log(2 % 1);//returns 0
+
+// //Try to make a variable that is assigned an opperation and console log it
+// //example: var num = 1 + 1
+
+// var number = 3 + 4;
+// console.log(number);
+
+
+// //Try to assign numbers to different variables and perform an 
+// //opperation to them - example var a - 5; var b = 10; console.log(a*b);
+
+// var a = 250;
+// var b = 300;
+
+// console.log (a+b);
+
+//Try to assign numbers to two different variables then assign any opperation on these two variables to a new variable
+
+
+// var c = 3;
+// var d = 5;
+// var e = c + d;
+
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+var newNum = 10;
+newNum = newNum + 1;
+console.log(newNum); //returns 11
+
+newNum++; //shortcut for newNum = newNum + 1;
+console.log(newNum);//returns 12
+
+newNum = newNum - 1;
+console.log(newNum); //returns 11
+
+newNum--;
+console.log(newNum); //returns 10
+
+newNum = newNum + 10;
+console.log(newNum); //returns 20
+
+newNum += 10; //shortcut for writing newNum = newNum + 10;
+console.log(newNum); //returns 30
+
+
+// newNum = newNum - 20;
+// console.log(newNum);
+
+newNum -= 20;
+console.log(newNum);
+
+
+
+
+
+
