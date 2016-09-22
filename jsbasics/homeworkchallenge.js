c7c00fcb657ea7326800d22e0187a7f24f0ab999
Sydney Ellen Haggard <Aussie@Sydneys-MacBook-Air.local>
2016-09-22 09:39:07 -0400

    JS functions and storefront project

--- jsbasics/homeworkchallenge.js
+++ jsbasics/homeworkchallenge.js
@@ -1,14 +1,24 @@
 //bronze
 
+//how i did my code
 function antiqueNameBuy (x,y) {
 	var antiqueOne = "Jumpsuit";
 	var antiqueTwo = "Typewriter";
 	console.log(antiqueOne + " and " + antiqueTwo);
 }
-
 antiqueNameBuy();
 
+//J&P code - correct code
+
+function combineWords (wordOne, wordTwo){
+	return wordOne + wordTwo;
+};
+
+console.log(combineWords("Red ", "Blue"));
+
 //silver
+
+//my code
 
 function calcBillPay (x) {
 	var utilitiesBill = 450;
@@ -20,6 +30,17 @@ function calcBillPay (x) {
 
 calcBillPay();
 
+//j&p code - correct code
+
+function monthlyBills(rent, phone, dateNight){
+	return (rent, phone, dateNight);
+}
+
+console.log(monthlyBills(200, 50, 1000));
+
+
+
+
 //gold
 
 function purchaseSodaCost (x, y, z){
@@ -29,4 +50,13 @@ function purchaseSodaCost (x, y, z){
 console.log(100 + " sodas will cost you $" + purchaseSodaCost(100, .99, 6.93));
 
 
+//J&P code -- correct code
 
+
+function calculatePrice(quantity, price, item, tax) {
+	// var tax = 0.07%;
+	var totalTax = (quantity * price * tax);
+	var totalPrice = quantity * price + totalTax;
+	return quantity + " " + " will cost you $" + totalPrice;
+}
+console.log(calculatePrice(132, .88, "cups of coffee", 1.07));
